829df93c2863ef5481eeb3c9ba310a915db3b529
Josef Vacek <vacekj@outlook.com>
2017-09-01 20:11:17 +0200

    Fix encoding problems original source still uses windows-1250 encoding *sigh*

--- src/supl.js
+++ src/supl.js
@@ -1,58 +1,61 @@
 const $ = require('cheerio');
 const parseTable = require('./cheerio-tableparser');
-const Axios = require('Axios');
+const req = require('request');
 const moment = require('moment');
-const decode = require('windows-1250').decode;
+const iconv = require('iconv-lite');
 
 const URL_PROXY = 'https://cors-anywhere.herokuapp.com/';
 const URL_SUPL = 'suplovani.gytool.cz/';
 const URL_ROZVRH = 'rozvrh.gytool.cz/index_Trida_Menu.html';
 const URL_DATES = URL_SUPL + '!index_menu.html';
 
-var axios = Axios.create({
-	baseURL: URL_PROXY,
+var request = req.defaults({
+	baseUrl: URL_PROXY,
 	timeout: 10000,
-	headers: { 'X-Requested-With': 'gytool.cz' }, // Also set the origin to gytool
-	transformResponse: [function (data) {
-		return decode(data);
-	}]
+	headers: { 'X-Requested-With': 'gytool.cz' },
+	encoding: null
 });
 
 function getClasses() {
 	return new Promise((resolve, reject) => {
-		axios.get(URL_ROZVRH).then((res) => {
-			if (res.status != 200) {
-				throw new URIError('Error loading rozvrh page | Code: ' + res.status);
+		request(URL_ROZVRH, (err, res, b) => {
+			if (err) {
+				reject(err);
 			}
-			let options = $(res.data).find('option');
+			let body = iconv.decode(b, 'win1250');
+			let options = $(body).find('option');
 			let values = options.map((i, option) => {
 				return $(option).text();
 			});
 			let classes = values.toArray();
 			resolve(classes);
-		}).catch((err) => {
-			reject(err);
 		});
 	});
 }
 
 function getSuplovani(date_url) {
 	return new Promise((resolve, reject) => {
-		axios.get(URL_SUPL + date_url).then((res) => {
-			let page = $(res.data);
+		request(URL_SUPL + date_url, (err, res, b) => {
+			if (err) {
+				reject(err);
+			}
+			let body = iconv.decode(b, 'win1250');
+			let page = $(body);
 			let suplovani_table = $(page).find('div:contains("Suplování")').next();
 			let supl = parseTable($, suplovani_table);
 			resolve(supl);
-		}).catch((err) => {
-			reject(err);
 		});
 	});
 }
 
 function getDates() {
 	return new Promise((resolve, reject) => {
-		axios.get(URL_DATES).then((res) => {
-			let options = $(res.data).find('option');
+		request(URL_DATES, (err, res, b) => {
+			if (err) {
+				reject(err);
+			}
+			let body = iconv.decode(b, 'win1250');
+			let options = $(body).find('option');
 			let data = options.map((i, option) => {
 				return {
 					url: $(option).attr('value'),
@@ -60,8 +63,6 @@ function getDates() {
 				};
 			});
 			resolve(data.toArray());
-		}).catch((err) => {
-			reject(err);
 		});
 	});
 }
