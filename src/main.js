cedcc600bb32f48efb1adef1336d231210c1f789
Josef Vacek <vacekj@outlook.com>
2017-09-03 12:17:59 +0200

    Updates

--- src/main.js
+++ src/main.js
@@ -6,21 +6,49 @@ require('bootstrap-loader');
 
 let $ = require('jquery');
 let supl = require('./supl');
+let moment = require('moment');
 
 $(document).ready(() => {
-	supl.getClasses().then(result => {
-		var option = '';
-		for (var i = 0; i < result.length; i++) {
-			option += '<option value="' + result[i].url + '">' + result[i].date + '</option>';
-		}
-		$('#selector_class').append(option);
-	});
+	render();
 
-	supl.getDates().then(result => {
-		console.log(result);
-		supl.getSuplovani(result[0].url).then((suplres) => {
-			console.log(supl.parseSuplovani(suplres));
-		});
-	});
 
 });
+
+function render() {
+	updateClasses();
+	updateDate();
+}
+
+function updateDate() {
+	supl.getDates().then(result => {
+		// Set current value
+		let today = moment().format('YYYY-MM-DD');
+		$('#selector_date').val(today);
+
+		// Set Max and Min value
+		let sorted = result.map(res => res.date).sort(function (a, b) {
+			return a - b;
+		});
+		let min = sorted[0];
+		let max = sorted[sorted.length - 1];
+		$('#selector_date').attr({
+			"max": max.format('YYYY-MM-DD'),
+			"min": min.format('YYYY-MM-DD')
+		});
+	});
+}
+
+function updateClasses() {
+	supl.getClasses().then(result => {
+		let html = ClassesToOptions(result);
+		$('#selector_class').append(html);
+	});
+}
+
+function ClassesToOptions(classes) {
+	var option = '';
+	for (var i = 0; i < classes.length; i++) {
+		option += '<option value="' + classes[i] + '">' + classes[i] + '</option>';
+	}
+	return option;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ module.exports = {
 	devServer: {
 		contentBase: './dist'
 	},
+	devtool: 'eval-source-map',
 	node: {
 		fs: 'empty',
 		net: 'empty',
