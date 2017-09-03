35627420414065e1cf4223e65cc96da6a962135c
Josef Vacek <vacekj@outlook.com>
2017-09-03 16:26:17 +0200

    Fix

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,6 @@
 const merge = require('webpack-merge');
-const MinifyPlugin = require("babel-minify-webpack-plugin");
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	plugins: [
-		new MinifyPlugin()
-	]
+
 });
