fbbfa5cbce471dd6074a7f5eb5b43f8b5c8a4f4c
Josef Vacek <vacekj@outlook.com>
2017-09-03 16:46:19 +0200

    Add further optimization to prod build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,23 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
+var CompressionPlugin = require("compression-webpack-plugin");
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
 
 module.exports = merge(common, {
-
+	plugins: [
+		new ClosureCompilerPlugin({
+			compiler: {
+				language_in: 'ECMASCRIPT6',
+				language_out: 'ECMASCRIPT5'
+			},
+			concurrency: 3,
+		}),
+		new CompressionPlugin({
+			asset: "[path].gz[query]",
+			algorithm: "gzip",
+			test: /\.(js|html)$/,
+			threshold: 10240,
+			minRatio: 0.8
+		})
+	]
 });
