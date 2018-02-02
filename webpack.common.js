b598325ecc8f7b6c9244caf915012c210bfa6033
Josef Vacek <vacekj@outlook.com>
2018-02-02 19:51:13 +0100

    add Babel polyfills

--- webpack.common.js
+++ webpack.common.js
@@ -1,9 +1,7 @@
 const path = require('path');
 
 module.exports = {
-	entry: {
-		main: './src/main.ts',
-	},
+	entry: ['@babel/polyfill', './src/main.ts'],
 	output: {
 		filename: 'bundle.js',
 		path: path.resolve(__dirname, 'dist')
