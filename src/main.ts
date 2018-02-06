1500f21f09500d76da1dcb0a6f93e51c167c9dba
Josef Vacek <vacekj@outlook.com>
2018-02-06 19:48:11 +0100

    Fix loading spinner background

--- src/main.ts
+++ src/main.ts
@@ -268,7 +268,7 @@ function removeControlChars(s: string) {
 function showLoadingIndicator() {
 	const indicator = (colspan) => `
 	<tr data-test="loadingIndicator">
-		<td colspan="${colspan}">
+		<td colspan="${colspan}" class="loading">
 			<svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
 				<circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle></svg>
 		</td>
