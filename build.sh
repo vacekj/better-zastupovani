3fdcc69f9de6febbbf5d3ad44488c914b70571f2
Josef Vacek <vacekj@outlook.com>
2018-02-09 12:30:26 +0100

    Another change

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
-echo "$TRAVIS_EVENT_TYPE"
+
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]
 then
-  	npm run e2e && exit 0
+  	npm run e2e
 else
-	npm run test && exit 0
+	npm run test
 fi
