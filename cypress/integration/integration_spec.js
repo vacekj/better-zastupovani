d4dd51b9a66e10e1b3928f298c1dbc648e2ea809
Josef Vacek <vacekj@outlook.com>
2018-03-02 19:31:06 +0100

    Add first few tests

--- cypress/integration/integration_spec.js
+++ cypress/integration/integration_spec.js
@@ -9,12 +9,55 @@ function test(testID) {
 
 describe("Integration Tests", () => {
 	it("loads successfully", () => {
-		cy.visit(process.env.DEV_SERVER_HOST || "192.168.1.200");
+		cy.visit(process.env.DEV_SERVER_HOST || "192.168.1.200:8080");
 	});
 
 	describe("Date buttons", () => {
-		it("should display today button", () => {
-			cy.get(test("button_today")).should("exist");
+		beforeEach(() => {
+			cy.get(test("button_today")).as("todayButton");
+			cy.get(test("button_tomorrow")).as("tomorrowButton");
+
+			cy.get("[data-test=datePicker] > option").as("option");
+		});
+
+
+		it("displays buttons", () => {
+			cy.get("@todayButton").should("exist");
+			cy.get("@tomorrowButton").should("exist");
+		});
+
+		// FIXME: maybe a bug with Cypress
+		// it("disables buttons based on date", () => {
+		// 	// Weekend
+		// 	const weekend = new Date(2017, 3, 5).getTime();
+		// 	cy.clock(3000);
+		// 	cy.reload(true)
+		// 		.get("@todayButton").should("have.attr", "disabled");
+
+		// 	// Friday
+		// 	cy.clock(new Date(2018, 3, 2).getTime());
+		// 	cy.reload()
+		// 		.get("@tomorrowButton").should("have.attr", "disabled");
+		// });
+	});
+
+	describe("Date Picker", () => {
+		beforeEach(() => {
+			cy.get(test("datePicker")).as("datePicker");
+		});
+
+		it("displays datePicker", () => {
+			cy.get("@datePicker").should("exist");
+		});
+
+		it('changes data on datePicker date change', () => {
+			cy.get(test("suplovaniTable")).then((el) => {
+				return el[0].innerHTML;
+			}).then((innerHTML) => {
+				return sha256(innerHTML);
+			}).then((hash) => {
+				console.log(hash);
+			});
 		});
 	});
 });
