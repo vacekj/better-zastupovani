660dd1a8a0ac2d785c16029794657dbca81687cc
Josef Vacek <vacekj@outlook.com>
2018-03-11 16:40:11 +0100

    Move images into separate folder

--- src/commonImports.ts
+++ src/commonImports.ts
@@ -8,11 +8,11 @@ import "whatwg-fetch";
 import "./bootstrap-md.min.css";
 
 // Webpack imports
-import "./favicon.png";
+import "./img/favicon.png";
 import "./robots.txt";
 
 // Images
-import "./gh.png";
-import "./pecet.jpg";
+import "./img/gh.png";
+import "./img/pecet.jpg";
 import "./svg/code.svg";
 import "./svg/heart.svg";
