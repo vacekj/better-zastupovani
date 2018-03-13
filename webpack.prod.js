d183555210c9543800e615f6fb0e8140df6c28ba
Josef Vacek <vacekj@outlook.com>
2018-03-13 19:31:20 +0100

    Add sourcemaps creating

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const ClosureCompilerPlugin = require('webpack-closure-compiler');
+const SentryCliPlugin = require('@sentry/webpack-plugin');
 
 module.exports = merge(common, {
 	module: {
@@ -17,6 +18,11 @@ module.exports = merge(common, {
 	plugins: [
 		new CleanWebpackPlugin(["dist/"]),
 		new ExtractTextPlugin("[name].css"),
+		new SentryCliPlugin({
+			include: './dist',
+			ignore: ['node_modules', 'webpack.config.js'],
+			configFile: '.sentryclirc'
+		}),
 		new ClosureCompilerPlugin({
 			compiler: {
 				language_in: 'ECMASCRIPT_2017',
