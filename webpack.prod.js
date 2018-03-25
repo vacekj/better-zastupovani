c14274ab9961928c9a11b9181bf9036823c8e996
Josef Vacek <vacekj@outlook.com>
2018-03-25 17:02:31 +0200

    Fix uglifyJS

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,7 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
 	mode: "production",
@@ -13,15 +13,18 @@ module.exports = merge(common, {
 			use: [MiniCssExtractPlugin.loader, "css-loader"]
 		}]
 	},
+	optimization: {
+		minimizer: [
+			new UglifyJsPlugin({
+				cache: true,
+				parallel: true,
+				uglifyOptions: {
+					ecma: 8
+				}
+			}),]
+	},
 	plugins: [
 		new CleanWebpackPlugin(["dist/"]),
-		// new UglifyJsPlugin({
-		// 	cache: true,
-		// 	parallel: true,
-		// 	uglifyOptions: {
-		// 		ecma: 8
-		// 	}
-		// }),
 		new MiniCssExtractPlugin({
 			filename: "[name].css",
 			chunkFilename: "[id].css"
