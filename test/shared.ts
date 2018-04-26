e90e88199a95fb351d71b8d81ad919c50efab8b6
Josef Vacek <vacekj@outlook.com>
2018-04-26 16:59:03 +0200

    fix(tests): classes not passing regex

--- test/shared.ts
+++ test/shared.ts
@@ -1,5 +1,5 @@
 export const regexes = {
-	trida: new RegExp("[IX|IV|V?I{0,3}]\.[A-C][1-8]?"),
+	trida: new RegExp("[IV|V?I]{0,4}\.[A-C][1-8]?|G[3-8]{6}"),
 	ucebna: new RegExp("[1-8]\.[A-C][1-8]?d?|[ABC].{3}d?"),
 	containsDate: new RegExp("pondělí|úterý|středa|čtvrtek|pátek")
 };
